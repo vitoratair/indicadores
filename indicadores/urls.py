b8d677e49b99059d724f9f90e71efd58ec0b138d
Vitor Atair <vitor.ruts@gmail.com>
2014-09-08 14:02:16 -0300

    Alterado menus para app indicador

--- indicadores/urls.py
+++ indicadores/urls.py
@@ -8,6 +8,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^core/', include('indicadores.core.urls', namespace='core')),
+    url(r'^indicador/', include('indicadores.indicador.urls', namespace='indicador')),
     url(r'', include('indicadores.login.urls', namespace='login')),
 )
 
