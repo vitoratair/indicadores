924a5756b2e22e2b7ddfcb33da868393e2235401
Vitor Atair <vitor.ruts@gmail.com>
2014-09-09 17:49:05 -0300

    Adicionado rota e view para página exemplo de segmento

--- indicadores/core/urls.py
+++ indicadores/core/urls.py
@@ -2,5 +2,6 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('indicadores.core.views',
+    url(r'segmento/$', 'segmento', name='home'),
     url(r'', 'home', name='home'),
 )
--- indicadores/core/views.py
+++ indicadores/core/views.py
@@ -5,3 +5,6 @@ def home(request):
 
     return render(request, 'core/index.html')
 
+def segmento(request):
+
+	return render(request, 'core/segmento.html')
